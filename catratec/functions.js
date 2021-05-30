37c8cb1a76b51f11b41a44169ccc74a53cd434ff
Nicolas Mauricio Martins Coiado <48792614+NicolasCoiado03@users.noreply.github.com>
2021-05-29 23:07:17 -0300

    V1
    Depositando todos os arquivos do projeto.

---
+++ catratec/functions.js
@@ -0,0 +1,8 @@
+function OnMouseLogo(){
+    document.getElementById("logo").src="imagens/catratec-logo-color.png";
+}
+
+function OutMouseLogo(){
+    document.getElementById("logo").src="imagens/catratec-logo-branco.png";
+}
+
